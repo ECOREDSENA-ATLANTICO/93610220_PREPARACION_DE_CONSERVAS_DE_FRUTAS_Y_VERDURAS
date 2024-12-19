5d860847d7346f12b4b08fc91349b74b7b31b17d
Liborio Castañeda Valencia <74385144+LiborioCastaneda99@users.noreply.github.com>
2024-12-19 16:01:05 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -93,7 +93,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CF01_93610220_DU.pdf',
       },
       {
         icono: 'fas fa-download',
